b31c3c53d76afeda824fe50f56061f8867bd5cc4
TheVinh <binkool281132442@gmail.com>
2020-09-29 20:21:38 +0700

    Updates

---
+++ precache-manifest.9ed0d4e1b0e999ecaef63b9864a1caff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68c01afd92b9649fa39fb991118ca24c",
+    "url": "/reactFreshweb/index.html"
+  },
+  {
+    "revision": "3099766d34db86d5ce96",
+    "url": "/reactFreshweb/static/css/main.e89f950f.chunk.css"
+  },
+  {
+    "revision": "538f8f4370d9e2676aa3",
+    "url": "/reactFreshweb/static/js/2.044a5b04.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactFreshweb/static/js/2.044a5b04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3099766d34db86d5ce96",
+    "url": "/reactFreshweb/static/js/main.a2448f98.chunk.js"
+  },
+  {
+    "revision": "94ccda4bef7ee4088ddb",
+    "url": "/reactFreshweb/static/js/runtime-main.5acc29a7.js"
+  }
+]);
